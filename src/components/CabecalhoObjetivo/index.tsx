0f222b4a5bc102ffc20d2d6135e0646e56fabe03
Roberta Lima <robertataisaa@gmail.com>
2021-11-17 17:45:01 -0300

    Iniciando modais de lançamento e de objeitvos

--- src/components/CabecalhoObjetivo/index.tsx
+++ src/components/CabecalhoObjetivo/index.tsx
@@ -1,17 +1,28 @@
 import { Container } from "./style";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoneyCheckAlt } from "@fortawesome/free-solid-svg-icons";
-
+import { Dialog } from "@material-ui/core";
+import { useState } from "react";
+import FormObjetivo from "../ModalObjetivo";
 const element = <FontAwesomeIcon icon={faMoneyCheckAlt} />;
 
 const CardCabecalhoObjetivo = () => {
+  const [insertModal, setInsertModal] = useState(false);
+  const handleClickInsertModal = () => setInsertModal(true);
+  const handleClickCloseInsertModal = () => setInsertModal(false);
+
   return (
     <Container>
       <div className="cabecalhoObjetivos">
         <div>
           <h3>Objetivos</h3>
         </div>
-        <div>{element}</div>
+        <div onClick={handleClickInsertModal}>{element}</div>
+        <Dialog open={insertModal} onClose={handleClickCloseInsertModal}>
+          <FormObjetivo
+            handleClickCloseInsertModal={handleClickCloseInsertModal}
+          />
+        </Dialog>
       </div>
     </Container>
   );
--- src/components/ExtratoDetalhado/index.tsx
+++ src/components/ExtratoDetalhado/index.tsx
@@ -3,12 +3,17 @@ import CardExtratoResumido from "../CardExtratoResumido";
 import MenuDetalharPeriodo from "../MenuDetalharPeriodo";
 import { BsPlusCircle } from "react-icons/bs";
 import { ExtratoDetalhadoContainer } from "./styles";
-
+import { Dialog } from "@material-ui/core";
+import { useState } from "react";
+import FormLancamento from "../ModalLancamentos";
 interface DetalhadoProps {
   setExtrato: (value: boolean) => void;
 }
 
 const ExtratoDetalhado = ({ setExtrato }: DetalhadoProps) => {
+  const [insertModal, setInsertModal] = useState(false);
+  const handleClickInsertModal = () => setInsertModal(true);
+  const handleClickCloseInsertModal = () => setInsertModal(false);
   return (
     <ExtratoDetalhadoContainer>
       <MenuDetalharPeriodo />
@@ -22,9 +27,14 @@ const ExtratoDetalhado = ({ setExtrato }: DetalhadoProps) => {
       <CardExtratoResumido />
       <CardExtratoResumido nome="Objetivo" tipo="objetivo" />
       <CardExtratoResumido nome="Salário" tipo="entrada" />
-      <button className="iconContainer">
+      <button className="iconContainer" onClick={handleClickInsertModal}>
         <BsPlusCircle />
       </button>
+      <Dialog open={insertModal} onClose={handleClickCloseInsertModal}>
+        <FormLancamento
+          handleClickCloseInsertModal={handleClickCloseInsertModal}
+        />
+      </Dialog>
       <Button secondary fullWidth onClick={() => setExtrato(false)}>
         Voltar
       </Button>
